442913fc608a7bc3baf47e43fd175be02bce4481
peterrobert <pwambui93@gmail.com>
2020-06-18 21:59:35 +0300

    created the transaction form

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,6 +1,35 @@
 class TransactionsController < ApplicationController
+    before_action :authenticate_user!
+
     def index
-        
+        @transaction = Transaction.all
+    end
+
+    def new
+        @transaction = Transaction.new
+    end
+
+    def create
+        @transaction = current_user.transactions.new(transaction_params)
+    
+        if @transaction.save
+
+          redirect_to transactions_path
+
+        else
+
+          render :new
+
+        end
+
+    end
+    
+
+
+    private
+
+    def transaction_params
+      params.require(:transaction).permit(:name, :amount)
     end
     
 end
