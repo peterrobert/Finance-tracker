364eaffac59ef25943f8e945185eb14339abb722
peterrobert <pwambui93@gmail.com>
2020-06-18 22:19:08 +0300

    created the relationship btn group and user

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,3 @@
 class Group < ApplicationRecord
+    belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :transactions
+  has_many :groups
 end
