27505912eaee91410207c14060237d1ad8aa166b
peterrobert <pwambui93@gmail.com>
2020-06-23 14:48:49 +0300

    fixed the design issue transaction page

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -11,6 +11,8 @@ class GroupsController < ApplicationController
 
     def show
         @group = current_user.groups.find(params[:id])
+        @transaction = @group.transactions
+        @total = @transaction.sum(:amount)
     end
 
     def create
