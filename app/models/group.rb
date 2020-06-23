5d72ec44a1976e2041ad445e3466e94641dd7ac8
peterrobert <pwambui93@gmail.com>
2020-06-23 19:41:23 +0300

    worked on the validation

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,7 @@
 class Group < ApplicationRecord
     belongs_to :user
     has_many :transactions
+
+    validates :name, presence: true, length: { maximum: 20 }
+    validates :icon, presence: true
 end
