103647e8434135f21e36bb285f1de3280d2c2fa2
peterrobert <pwambui93@gmail.com>
2020-06-23 18:35:35 +0300

    removed the delete buttons for none members

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,3 +1,15 @@
 module GroupsHelper
-   
+   def edit_delete_btn(user, group)
+
+    if group.user_id == user.id
+    links = [link_to("edit",  edit_group_path(group), class: "btn btn-primary edit_btn"), link_to("delete", group_path(group), class: "btn btn-danger delete_grp_btn", data: {confirm: "Are you sure?"}, method: :delete)]
+      
+    content_tag (:span) do
+    links.collect{
+        |l| concat content_tag(:span, l)
+    }
+    end
+    end
+
+   end
 end
